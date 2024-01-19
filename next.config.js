34d0469651aa34c003d000d794487bf06baa9f47
Suat-17 <yargicisuat@gmail.com>
2024-01-20 02:59:10 +0300

    chore: last change

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    experimental: {
-      appDir: true
-    },
-    images:{
-      domains: ['upload.wikimedia.org', 'lh3.googleusercontent.com']
-    }
-  }
-  
-  module.exports = nextConfig
+  output: "export",
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ["upload.wikimedia.org", "lh3.googleusercontent.com"],
+  },
+};
+
+module.exports = nextConfig;
